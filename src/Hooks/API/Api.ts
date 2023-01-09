d2c65eddf961b096c4ffa483bec1a566d087e5b5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-09 12:45:52 +0100

    Created a util function for passing returned data from api call

--- src/Hooks/API/Api.ts
+++ src/Hooks/API/Api.ts
@@ -1,10 +1,16 @@
-import { DifficultyState } from 'Types/Types';
+import { ShuffleArray } from './../../utils/ShuffleArray';
+import { DifficultyState, Question, QuestionState } from 'Types/Types';
 
 
 export const FetchApi = async (amount:number, difficulty : DifficultyState) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty-${difficulty}&type=multiple`;
-    const returnData = await (await fetch(endpoint)).json();
-
-    console.log(returnData);
+    const returnData = await (await fetch(endpoint)).json();   
+    return returnData.results.map((question : Question)=> (
+        {
+            ...question,
+            answers : ShuffleArray([...question.incorrect_answers, question.correct_answer])
+        }
+        ))
+    
     
 }
