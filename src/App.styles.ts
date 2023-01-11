7838a14ac063b5b2340dfd4ed099600c0dd1526d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-11 15:22:28 +0100

    working on styels

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -8,6 +8,15 @@ html {
 
 body {
 background: linear-gradient(90deg, hsla(221, 45%, 73%, 1) 0%, hsla(220, 78%, 29%, 1) 100%);
+backgound-size : cover;
+margin : 0;
+padding : 0 20px;
+display : flex;
+justify-content : center;
 
 
+* {
+    box-sizing : border-box;
+    font-family : "Catamaran", sans-serif;
+}
 `
