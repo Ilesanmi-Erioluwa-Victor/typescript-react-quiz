8224ef9194ae7bd33badb078cd32611070cd05ac
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-11 15:20:27 +0100

    working on styels

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,1 +1,13 @@
 import styled, { createGlobalStyle } from "styled-components"
+
+export const GlobalStyle = createGlobalStyle `
+
+html {
+    height : 100%;
+}
+
+body {
+background: linear-gradient(90deg, hsla(221, 45%, 73%, 1) 0%, hsla(220, 78%, 29%, 1) 100%);
+
+
+`
