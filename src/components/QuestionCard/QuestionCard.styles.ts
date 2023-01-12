c9f1b34e31f9221c17fb73729504eef853164a5d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-12 09:35:16 +0100

    Almost done with project

--- src/components/QuestionCard/QuestionCard.styles.ts
+++ src/components/QuestionCard/QuestionCard.styles.ts
@@ -18,6 +18,26 @@ type ButtonWrapperProps = {
 }
 
 export const ButtonWrapper = styled.div<ButtonWrapperProps>`
+ transition : all 0.3s ease-in-out;
 
+ :hover {
+    opacity : 0.8;
+ }
+ 
+ button {
+    cursor: pointer;
+    user-select : none;
+    font-size : 0.8rem;
+    margin : 5px 0;
+    width : 100%;
+    height : 40px;
+    background : ${({correct, userClicked}) => 
+    correct ? "linear-gradient(90deg,#56ffa4, #59bc86)" : 
+    !correct && userClicked ? "linear-gradient(90deg,#ff5656, #c16868)": "linear-gradient(90deg,#56ccff, #6eafb4)"};
+ }
+ border: 3px solid white;
+ box-shadow : 1px 2px 0px rgba( 0, 0, 0, 0.1 );
+ border-radius : 10px;
+ color : white;
 
 `
--- src/components/QuestionCard/QuestionCard.tsx
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { Props } from 'Types/Types'
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
 
 const QuestionsCard:React.FC<Props> = ({ question,answers,callback,userAnswer,questionNum, totalQuestions}) => {
   return (
-    <div>
+    <Wrapper>
       <p className='number'>Question : {questionNum} / {totalQuestions}</p>
 
       <p dangerouslySetInnerHTML={{__html : question}} />
       <div>
        {answers.map((answer, i) => (
-        <div key={i}>
+        <ButtonWrapper key={i}
+         correct={userAnswer?.correctAnswer === answer}
+         userClicked={userAnswer?.answer === answer}
+        >
             <button disabled={!!userAnswer} onClick={callback} value={answer}>
               <span dangerouslySetInnerHTML={{__html : answer}} />
             </button>
-        </div>
+        </ButtonWrapper>
        ))}
       </div>
-    </div>
+    </Wrapper>
   )
 }
 
