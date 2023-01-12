9fb97a81c1c1dd433b41c6f3f449cfb5f28c3bf2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-01-12 08:26:26 +0100

    Working on styles

--- src/components/QuestionCard/QuestionCard.styles.ts
+++ src/components/QuestionCard/QuestionCard.styles.ts
@@ -5,5 +5,10 @@ export const Wrapper = styled.div`
  background-color : #ebfeff;
  border-radius : 10px;
  border: 2px solid #0085a3;
- box-shadow : 0px 5px 10px rgba( 0, 0, 0, 0.25)
+ box-shadow : 0px 5px 10px rgba( 0, 0, 0, 0.25);
+ text-align : center;
+
+ p {
+    font-size : 1rem;
+ }
 `
